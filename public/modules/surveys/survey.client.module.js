d64b335beb95b4987e5bb64923dbb0f2d0d382f3
alweky <alweky@yahoo.ca>
2015-04-09 09:15:46 -0400

    changed module to surveys

--- public/modules/surveys/survey.client.module.js
+++ public/modules/surveys/survey.client.module.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Use Applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('Surveys');
+ApplicationConfiguration.registerModule('surveys');

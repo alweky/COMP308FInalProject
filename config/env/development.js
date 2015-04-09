252520597f380db93ebc1db5b969ab246946c79b
alweky <alweky@yahoo.ca>
2015-04-08 22:42:35 -0400

    just changed mongodb address

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/survey-app-dev',
+	//db: 'mongodb://localhost/survey-app-dev',
+	db: 'mongodb://<dbuser>:<dbpassword>@ds049631.mongolab.com:49631/heroku_app35695970',
 	app: {
 		title: 'Survey App - Development Environment'
 	},
